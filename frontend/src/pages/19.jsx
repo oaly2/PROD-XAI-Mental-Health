de06b8ae4394ddb961c2edb181a74d49b61973a2
Omar Osama <omarosama2114@gmail.com>
2024-06-03 16:31:59 +0200

    Updated button placement

--- frontend/src/pages/19.jsx
+++ frontend/src/pages/19.jsx
@@ -49,7 +49,8 @@ export default function IntroductoryPage() {
         <br/>
  
         </p>
-
+        <br/>
+        <br/>
         <Button
               variant="contained"
               onClick={handleProceed}
