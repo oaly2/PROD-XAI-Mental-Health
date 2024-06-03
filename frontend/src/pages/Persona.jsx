bd895b9dabf0c8a0fdd08e36a5c6b58f188adc52
Omar Osama <omarosama2114@gmail.com>
2024-06-03 19:20:25 +0200

    added explanation image

--- frontend/src/pages/Persona.jsx
+++ frontend/src/pages/Persona.jsx
@@ -4,7 +4,6 @@ import explanationsData from '../240424_xaimh_output-finalized.json';
 import { useNavigate } from "react-router-dom";
 import styles from '../styles/PersonaPage.module.css';
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 
 export default function PersonaPage({ showProceedButton = true }) {
   const [explanation, setExplanation] = useState(null);
@@ -57,16 +56,6 @@ export default function PersonaPage({ showProceedButton = true }) {
     }
   };
 
-  const valueMapping = {
-    "Stark unterdurchschnittlich": "Sehr gering",
-    "Leicht unterdurchschnittlich": "Gering",
-    "Durchschnittlich": "Durchschnittlich",
-    "Leicht überdurchschnittlich": "Hoch",
-    "Stark überdurchschnittlich": "Sehr hoch"
-  };
-
-  const mappedValue = (value) => valueMapping[value] || value;
-
   if (!explanation) {
     return <div>Loading...</div>;
   }
@@ -78,66 +67,17 @@ export default function PersonaPage({ showProceedButton = true }) {
       </Helmet>
       <br/>
       <div className={styles.container}>
+        <h1 className={styles.subTitle}>
+          Versetzen Sie sich nun in das hypothetische Szenario. Stellen Sie sich vor, Sie nutzen eine Smart-Sensing-App für mentale Gesundheit – also eine App, die Sensordaten von Ihrem Smartphone nutzt, um Informationen zu Ihrer mentalen Gesundheit zu liefern  
+        </h1>
+        <br/>
+        <img src="..\assets\images\Explanation.png" alt="Explanation" />
+        <br/>
         {showProceedButton && (
-          <h1 className={styles.subTitle}>
-            Versetzen Sie sich nun in das hypothetische Szenario. Stellen Sie sich vor, Sie nutzen eine Smart-Sensing-App für mentale Gesundheit – also eine App, die Sensordaten von Ihrem Smartphone nutzt, um Informationen zu Ihrer mentalen Gesundheit zu liefern  
-            <br/> <br/>
-            Bitte merken Sie sich die folgenden Informationen, damit Sie die Studie erfolgreich fortsetzen können
-            <br/> <br/> <br/>
-            Stellen Sie sich vor, die App hätte folgende Informationen über Sie gesammelt:
-          </h1>
-        )}
-        {!showProceedButton && (
-          <h1 className={styles.subTitle}>
-           Zur Erinnerung: Die App hat folgende Informationen über Sie
-          </h1>  
-        )}
-        <br/>
-        <ul className={styles.list}>
-          {[
-            { label: 'Dein Stresslevel', value: mappedValue(explanation.Dein_Stresslevel) },
-            { label: 'Deine Schlafqualität'+ '\u00A0', value: mappedValue(explanation.Deine_Schlafqualitaet) },
-            { label: 'Deine Anzahl sozialer Kontakte'+ '\u00A0' .repeat(6), value:  mappedValue(explanation.Anzahl_deiner_sozialen_Kontakte) },
-            { label: 'Deine Qualität sozialer Kontakte' + '\u00A0' .repeat(7), value: mappedValue(explanation.Qualitaet_deiner_sozialen_Kontakte) },
-            { label: 'Deine Qualität der Ernährung'+ '\u00A0' .repeat(5), value: mappedValue(explanation.Qualitaet_deiner_Ernaehrung) },
-            { label: 'Deine sportliche Aktivität'+ '\u00A0' .repeat(4), value: mappedValue(explanation.Deine_sportliche_Aktivitaet) },
-            { label: 'Deine Zeit am Handy'+ '\u00A0', value: mappedValue(explanation.Zeit_am_Handy) },
-            { label: 'Deine Länge der Telefonate'+ '\u00A0'+ '\u00A0'+ '\u00A0'+ '\u00A0', value: mappedValue(explanation.Laenge_deiner_Telefonate) },
-            { label: 'Deine Mobilität', value: mappedValue(explanation.Deine_Mobilitaet) }
-          ].map((item, index) => (
-            <TextField 
-              key={index}
-              label={ '\u00A0'+ item.label + '\u00A0' .repeat(12) }
-              value={item.value || ''}
-              variant="outlined"
-              fullWidth
-              Read only
-              margin="normal"
-              padding="30px"
-              
-              InputLabelProps={{
-                style: { color: '#19b394', fontSize: '20px', fontWeight: 'bold'},
-                shrink: true
-              }}
-
-              inputProps={{
-                spellCheck: 'false',
-                style:{ boxShadow: 'none', color: 'black', cursor: 'default', marginTop: '15px', marginBottom: '15px', marginLeft: '10px', fontSize: '16px'} 
-              }}
-
-              sx={{
-                pointerEvents: 'none' // This will make the TextField not clickable
-              }}
-            />
-          ))}
-          <br/>
-        </ul>
-        <br/>
-        {showProceedButton&& (
           <>
-          <br/>
-          {message && <p className= {styles.list} style={{ color: 'red' }}>{message}</p>}
-          <br/>
+            <br/>
+            {message && <p className={styles.list} style={{ color: 'red' }}>{message}</p>}
+            <br/>
             <Button
               variant="contained"
               onClick={handleProceed}
@@ -145,7 +85,6 @@ export default function PersonaPage({ showProceedButton = true }) {
             >
               Weiter &#x279C;
             </Button>
-
           </>
         )}
       </div>
