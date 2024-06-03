4b9b28ff37c84bdeead4f30ef4276faaf526df26
Omar Osama <omarosama2114@gmail.com>
2024-06-03 21:01:25 +0200

    u

--- frontend/src/pages/CounterFactual/index.jsx
+++ frontend/src/pages/CounterFactual/index.jsx
@@ -121,13 +121,15 @@ export default function SurveyScreenDepressionCFPage() {
 
     return (
       <div style={{ textAlign: 'center' }}>
-        <Text as="p" className="text-center text-base md:text-xl mx-2 my-4" style={{ fontSize: '1.25em', lineHeight: '1.5em' }}>
+        <Text as="p" className="text-center text-base md:text-xl mx-2 my-4" style={{ fontSize: '1.25em', lineHeight: '1.75em' }}>
           Unter folgenden Umständen würde die KI ein {oppositePrediction} prognostizieren:
         </Text>
         <ul className={styles.listCenter}>
           {features.map((f, index) => (
-            <li key={index} style={{ color: "#15b1e2", fontWeight: "bold", fontSize: '18px', marginBottom: '10px' }}>
+            <li key={index} style={{ color: "#15b1e2", fontWeight: "bold", fontSize: '18px' }}>
               {f.feature} (aktuell {f.value}) müsste um {Math.abs(Math.round(f.percentage))}% {f.percentage >= 0 ? 'höher' : 'geringer'} sein
+              <br />
+              <br />
             </li>
           ))}
         </ul>
@@ -146,8 +148,8 @@ export default function SurveyScreenDepressionCFPage() {
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
 
-      <div className={styles.container} style={{padding: '10px'}}>  
-        <h1 className={styles.subTitle} style={{color: 'black', fontWeight: 'normal', marginBottom: '20px'}}>
+      <div className={styles.container} style={{padding:'15px 15px'}}>  
+        <h1 className={styles.subTitle} style={{color: 'black', fontWeight: 'normal'}}>
         Stellen Sie sich weiterhin vor, Sie nutzen eine
         Smart-Sensing-App für mentale Gesundheit –
         also eine App, die Sensordaten von Ihrem
@@ -159,9 +161,9 @@ export default function SurveyScreenDepressionCFPage() {
         </h1>
       </div>
 
-      <div className={styles.container} style={{padding: '10px'}}>  
-        <div className="bg-blue-50 rounded-lg mx-auto px-10 py-4">
-          <Text className="text-center text-xl md:text-2xl font-semibold mt-4" style={{ fontSize: '2.0em', marginBottom: '10px' }} >
+      <div className={styles.container} style={{padding:'15px 15px', marginTop: '-50%'}}>  
+        <div className="bg-blue-50 rounded-lg mx-auto px-20 py-6">
+          <Text className="text-center text-xl md:text-2xl font-semibold mt-4" style={{ fontSize: '2.0em' }} >
             Die KI prognostiziert auf Basis<br />deiner Smartphone-Daten<br /><br />   
           </Text>
           <Heading as="h2" className={`${explanation.prediction === "depression" ? "text-red-A700" : "text-green-600"} text-3xl md:text-5xl text-center`} style={{ fontSize: '2.5em' }}  >
@@ -176,16 +178,18 @@ export default function SurveyScreenDepressionCFPage() {
         </Text>
       </div>
 
-      <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+      <div style={{ marginBottom: '50px', marginTop: '-50%' }}>
+          <br/>
           {message && (
             <div className={styles.messageContainer}>
               <p className={styles.messageText}>{message}</p>
             </div>
           )}
+          <br/>
         <Button
           variant="contained"
           onClick={handleProceed}
-          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', marginTop: '20px'}}
+          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', width: '10%', marginLeft: '45%', marginBottom: '30px'}}
         > 
           Weiter &#x279C;
         </Button>
