30676673e1dd5e30f97932419077484a018001e7
Omar Osama <omarosama2114@gmail.com>
2024-06-03 16:20:29 +0200

    Style change

--- frontend/src/pages/Persona.jsx
+++ frontend/src/pages/Persona.jsx
@@ -96,14 +96,14 @@ export default function PersonaPage({ showProceedButton = true }) {
         <ul className={styles.list}>
           {[
             { label: 'Dein Stresslevel', value: mappedValue(explanation.Dein_Stresslevel) },
-            { label: 'Deine Schlafqualität', value: mappedValue(explanation.Deine_Schlafqualitaet) },
-            { label: 'Deine Anzahl sozialer Kontakte'+ '\u00A0' .repeat(5), value:  mappedValue(explanation.Anzahl_deiner_sozialen_Kontakte) },
+            { label: 'Deine Schlafqualität'+ '\u00A0', value: mappedValue(explanation.Deine_Schlafqualitaet) },
+            { label: 'Deine Anzahl sozialer Kontakte'+ '\u00A0' .repeat(6), value:  mappedValue(explanation.Anzahl_deiner_sozialen_Kontakte) },
             { label: 'Deine Qualität sozialer Kontakte' + '\u00A0' .repeat(7), value: mappedValue(explanation.Qualitaet_deiner_sozialen_Kontakte) },
             { label: 'Deine Qualität der Ernährung'+ '\u00A0' .repeat(5), value: mappedValue(explanation.Qualitaet_deiner_Ernaehrung) },
-            { label: 'Deine sportliche Aktivität'+ '\u00A0' .repeat(3), value: mappedValue(explanation.Deine_sportliche_Aktivitaet) },
-            { label: 'Deine Zeit am Handy', value: mappedValue(explanation.Zeit_am_Handy) },
+            { label: 'Deine sportliche Aktivität'+ '\u00A0' .repeat(4), value: mappedValue(explanation.Deine_sportliche_Aktivitaet) },
+            { label: 'Deine Zeit am Handy'+ '\u00A0', value: mappedValue(explanation.Zeit_am_Handy) },
             { label: 'Deine Länge der Telefonate'+ '\u00A0'+ '\u00A0'+ '\u00A0'+ '\u00A0', value: mappedValue(explanation.Laenge_deiner_Telefonate) },
-            { label: 'Deine Mobilität'+ '\u00A0', value: mappedValue(explanation.Deine_Mobilitaet) }
+            { label: 'Deine Mobilität', value: mappedValue(explanation.Deine_Mobilitaet) }
           ].map((item, index) => (
             <TextField 
               key={index}
