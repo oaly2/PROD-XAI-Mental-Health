07f30d3d0d37b8a8b54499c4d5d362cfe9a442ed
Omar Osama <omarosama2114@gmail.com>
2024-06-03 23:51:40 +0200

    Update index.jsx

--- frontend/src/pages/CounterFactual/index.jsx
+++ frontend/src/pages/CounterFactual/index.jsx
@@ -183,7 +183,7 @@ export default function SurveyScreenDepressionCFPage() {
         <Button
           variant="contained"
           onClick={handleProceed}
-          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', width: '90%'}}
+          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', width: '120%'}}
         > 
           Weiter &#x279C;
         </Button>
