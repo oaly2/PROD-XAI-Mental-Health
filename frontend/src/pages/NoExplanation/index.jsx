d3702ca86ce8ea165a0a75a55fb203c27febf4a9
Omar Osama <omarosama2114@gmail.com>
2024-06-04 00:02:40 +0200

    Update index.jsx

--- frontend/src/pages/NoExplanation/index.jsx
+++ frontend/src/pages/NoExplanation/index.jsx
@@ -80,7 +80,7 @@ export default function SurveyScreenDepressionPage({ explanation }) {
         <Button
           variant="contained"
           onClick={handleProceed}
-          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', width: '10%', marginLeft: '45%', marginBottom: '30px'}}
+          style={{ color: 'white', backgroundColor: '#19b394', fontWeight: 'bold', fontSize: '16px', padding: '10px 20px', width: '120%'}}
         > 
           Weiter &#x279C;
         </Button>
