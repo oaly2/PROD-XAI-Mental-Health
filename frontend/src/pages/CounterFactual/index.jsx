837392601706e2382fda5bb1689105fcfe7238c5
Omar Osama <omarosama2114@gmail.com>
2024-06-03 15:16:52 +0200

    Fixed message positioning

--- frontend/src/pages/CounterFactual/index.jsx
+++ frontend/src/pages/CounterFactual/index.jsx
@@ -148,7 +148,7 @@ export default function SurveyScreenDepressionCFPage({ explanation }) {
 
       <div style={{ marginBottom: '50px' }}>
           <br/>
-          {message && <p className= {styles.list} style={{ color: 'red' }}>{message}</p>}
+          {message && <p className= {styles.list} style={{ color: 'red', marginLeft: '45%' }}>{message}</p>}
           <br/>
         <Button
           variant="contained"
